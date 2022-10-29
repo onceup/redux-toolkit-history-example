11b270ea376c239d16c44cf545f96560a52db1a5
denis zaborev <denis.zaborev@appsfactory.de>
2022-10-30 00:27:33 +0200

    [ADD] add reverse

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
-import Circle from '@/components/Circle/Circle';
-import { idIterator } from '@/utils/idIterator';
-import { StoreDispatch, StoreState } from '@/context/store';
+import Circle from '../../components/Circle/Circle';
+import { idIterator } from '../../utils/idIterator';
+import { StoreDispatch, StoreState } from '../../context/store';
 import { newCurrentElement, replaceCurrentElement } from '../../context/element/elementSlice';
 import Rectangle from '../../components/Rectangle/Rectangle';
 import './styles.css';
